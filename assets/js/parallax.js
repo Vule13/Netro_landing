151b830692d773337bbf7019149fac3f1100dcd2
levu <vulecr13@gmail.com>
2024-10-31 17:19:54 +0700

    update code

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -14,7 +14,7 @@ window.addEventListener('scroll', function() {
   const parallaxElementsA = document.querySelectorAll('.banner-bottom__left');
   for (let i = 0; i < parallaxElementsA.length; i++) {
     let scrollPosition = window.scrollY;
-    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 150);
+    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 200);
     parallaxElementsA[i].style.transform = `perspective(1200px) translateY(-${offset}px) rotateX(3deg) rotateY(10deg)`;
   }
 
@@ -22,21 +22,21 @@ window.addEventListener('scroll', function() {
   const parallaxElementsB = document.querySelectorAll('.banner-bottom__right');
   for (let i = 0; i < parallaxElementsB.length; i++) {
     let scrollPosition = window.scrollY;
-    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 150);
+    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 200);
     parallaxElementsB[i].style.transform = `perspective(1200px) translateY(-${offset}px) rotateX(3deg) rotateY(-10deg)`;
   }
 
   const parallaxElementsC = document.querySelectorAll('.banner-bottom__left-small');
   for (let i = 0; i < parallaxElementsC.length; i++) {
     let scrollPosition = window.scrollY;
-    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 150);
+    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 200);
     parallaxElementsC[i].style.transform = `perspective(1200px) translateY(-${offset}px) rotateX(11deg) rotateY(15deg)`;
   }
   
   const parallaxElementsD = document.querySelectorAll('.banner-bottom__right-small');
   for (let i = 0; i < parallaxElementsD.length; i++) {
     let scrollPosition = window.scrollY;
-    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 150);
+    let offset = Math.min(scrollPosition * (0.2 + i * 0.05), 200);
     parallaxElementsD[i].style.transform = `perspective(1200px) translateY(-${offset}px) rotate(2deg) rotateX(8deg) rotateY(-11deg)`;
   }
 });
